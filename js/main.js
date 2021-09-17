50d7fba64a1da1cdb1cbeeb737d21c52a2535730
Vlad <66181660+Vladokos@users.noreply.github.com>
2021-09-17 23:55:25 +0400

    for prompt added js logic and style and stylized text

--- js/main.js
+++ js/main.js
@@ -16,3 +16,18 @@ burgerMenu.addEventListener("click", () => {
     checkClick = false;
   }
 });
+
+//айди кнопки для появления текста-подсказки
+//айди текста=подзказки
+const taskPrompt = document.getElementById("task__prompt");
+const prompt = document.getElementById("prompt");
+let checkOpen = false;
+taskPrompt.addEventListener("click", () => {
+  if (!checkOpen) {
+    prompt.classList.add("active");
+    checkOpen = true;
+  } else {
+    prompt.classList.remove("active");
+    checkOpen = false;
+  }
+});
