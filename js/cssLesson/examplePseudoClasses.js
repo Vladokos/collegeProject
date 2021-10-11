cebcb83ed14f9080657aa793a7bf0dd2ed3b80ab
Vlad <66181660+Vladokos@users.noreply.github.com>
2021-10-12 00:57:08 +0400

    add some theory in new lesson 9

---
+++ js/cssLesson/examplePseudoClasses.js
@@ -0,0 +1,9 @@
+stateExample = document.getElementById("stateExample");
+
+stateExample.addEventListener("mouseenter", () => {
+    stateExample.innerHTML = "color: #3F2F22;";
+});
+
+stateExample.addEventListener("mouseleave", () =>{
+    stateExample.innerHTML = "color: #f0e7d6;";
+});
