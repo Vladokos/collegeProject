0c93bc718e7a87070e88cb973e4602620b458b60
Vlad <66181660+Vladokos@users.noreply.github.com>
2021-10-16 23:58:36 +0400

    burger menu

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,9 +2,23 @@
 // получаем айди контента который будет выезжать
 const burgerMenu = document.getElementById("header__burgergMenu");
 const burgerMenuContent = document.getElementById("burgerMenu__content");
+const notActive = document.getElementsByClassName("notActiveContent");
 let checkClick = false;
 //добавляем слушателя к бургер меню и в зависимости от чекера меняем класс у контента
 //добавляем актив для крестика из бургер меню
+for (let i = 0; i < notActive.length; i++) {
+  notActive[i].addEventListener("mouseover", (event) => {
+    console.log(event);
+    console.log(event.target.offsetWidth);
+
+    let widh = event.target.offsetWidth + "px";
+    let stl = window.getComputedStyle(notActive[i], ":after").getPropertyValue("width");;
+    console.log(stl);
+    stl.setPropertyValu
+    
+  });
+}
+
 burgerMenu.addEventListener("click", () => {
   if (!checkClick) {
     burgerMenuContent.classList.add("active");
